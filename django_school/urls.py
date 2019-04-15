8425644cde885dc2fc4837016546125dee33cde2
anjaniraikar <anjaniraikar11@gmail.com>
2019-04-15 15:32:58 +0530

    updated full project

---
+++ django_school/urls.py
@@ -0,0 +1,15 @@
+from django.urls import include, path
+from django.contrib.auth import views as auth_views
+from classroom.views import classroom, students, teachers
+
+urlpatterns = [
+    path('', include('classroom.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup/', classroom.SignUpView.as_view(), name='signup'),
+    path('accounts/signup/student/', students.StudentSignUpView.as_view(), name='student_signup'),
+    path('accounts/signup/teacher/', teachers.TeacherSignUpView.as_view(), name='teacher_signup'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+]
