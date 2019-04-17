d779c929a2ccbf9a7d8f9e102ed83e7b5ff541ca
anjaniraikar <anjaniraikar11@gmail.com>
2019-04-17 19:52:11 +0530

    email

--- django_school/settings.py
+++ django_school/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'd$pxg6fisc4iwzk&vz^s_d0lkf&k63l5a8f!obktw!jg#4zvp3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['anjaniraikar.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -96,7 +96,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
